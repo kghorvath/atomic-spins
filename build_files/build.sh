99e7a4d550d3331f07f6eceb3386ea0456d670ac
Christian Glombek <lorbus@fedoraproject.org>
2025-06-10 06:17:21 +0200

    wip

--- build_files/build.sh
+++ build_files/build.sh
@@ -41,9 +41,8 @@ mv /tmp/rpms/* /tmp/akmods/
 cat /etc/dnf/dnf.conf
 cat /etc/yum.repos.d/*
 
-dnf5 --setopt=disable_excludes=*
 # Install Kernel
-dnf5 -y install \
+dnf5 --setopt=disable_excludes=* -y install \
     /tmp/kernel-rpms/kernel-[0-9]*.rpm \
     /tmp/kernel-rpms/kernel-core-*.rpm \
     /tmp/kernel-rpms/kernel-modules-*.rpm
