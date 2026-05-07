22740dac2e15abcceea3fdf1c983ae650f5ab58a
Kamin Horvath <kamin@kghorvath.com>
2026-05-06 23:56:09 -0400

    Added gnome-keyring, wget, vim

--- build_files/20-utils.sh
+++ build_files/20-utils.sh
@@ -5,7 +5,7 @@ set -ouex pipefail
 ### Install packages
 
 # Editors
-dnf5 -y install emacs neovim
+dnf5 -y install emacs neovim vim
 
 # Domain Joining
 dnf5 -y install adcli oddjob-mkhomedir realmd samba-common-tools samba-winbind sssd-ad sssd-ipa sssd-ldap libsss_autofs libsss_sudo sssd-nfs-idmap
@@ -14,7 +14,7 @@ dnf5 -y install adcli oddjob-mkhomedir realmd samba-common-tools samba-winbind s
 dnf5 -y install python3-pip
 
 # Utilities
-dnf5 -y install fastfetch htop nmtui rclone screen stow tmux
+dnf5 -y install fastfetch htop nmtui rclone screen stow tmux wget
 
 # Cockpit
 dnf5 -y install cockpit cockpit-podman cockpit-ostree
--- build_files/30-graphical.sh
+++ build_files/30-graphical.sh
@@ -10,3 +10,6 @@ dnf5 -y install ghostty
 dnf5 -y copr disable scottames/ghostty
 
 dnf5 -y install waydroid
+
+# Make sure GNOME Keyring is installed
+dnf5 -y install gnome-keyring
